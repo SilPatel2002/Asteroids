8948b95e67972eb91ed5b703625cbaa915f48037
SilPatel2002 <sil2002patel@gmail.com>
2025-03-23 01:40:51 -0400

    added a window, gameplay loop, and closing window event

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
+from constants import *
+
+
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()  #initialize all pygame modules
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # get a new GUI window
+
+    while True:  # infinite game loop 
+
+
+        for event in pygame.event.get(): #pygame.event.get() returns a list of inputs
+           if event.type == pygame.QUIT: # clicking the x is pygame.QUIT
+               return # return exits the main function effectively exiting the game
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
 
 
 
