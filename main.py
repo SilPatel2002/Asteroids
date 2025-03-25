5d9416f1c759ddd30b0df63b2ac7cf9b928b25fa
SilPatel2002 <sil2002patel@gmail.com>
2025-03-25 19:34:15 -0400

    added the appropriate classes and file to allow player movement

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 
 
 def main():
     pygame.init()  #initialize all pygame modules
 
+    clock = pygame.time.Clock()  # initialize an internal clock
+
+    dt = 0 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()   # create two groups to manage objects
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)   # initialize a player object
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # get a new GUI window
+
+    
+
+
+    
 
     while True:  # infinite game loop 
 
@@ -15,8 +32,24 @@ def main():
            if event.type == pygame.QUIT: # clicking the x is pygame.QUIT
                return # return exits the main function effectively exiting the game
 
-        screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0))  #fill the screen black
+
+
+        updatable.update(dt)
+
+        for obj in drawable:      # draw the player. pass the screen as the surface to print it on
+            obj.draw(screen)
+        
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
+        # this will pause the game loop until 1/60th of a second passes
+        # it also returns the milliseconds that have passed since the last time it was called. Divide by 1000 (ms to s) 
+
+
+
+
 
 
 
